161663169e93ba38cc6690edf39df8561b732795
Douglas Pereira <dougls99@outlook.com>
2021-10-30 16:48:27 -0300

    colocando infos do github

--- main.js
+++ main.js
@@ -19,7 +19,15 @@ changeSocialMediaLinks()
 function getGitHubProfileInfos() {
   const url = `https://api.github.com/users/${LinksSocialMedia.github}`
 
-  alert(url)
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
 }
 
 getGitHubProfileInfos()
